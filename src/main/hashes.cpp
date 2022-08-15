e599db4e07217bde9153372cd4e2d92a4279dddc
zriback <zacharythecdr@gmail.com>
2022-08-15 10:45:32 -0400

    Update error handling

--- src/main/hashes.cpp
+++ src/main/hashes.cpp
@@ -8,18 +8,22 @@
 void hash_md5(const char *hashTypeStr, const unsigned char *message, size_t message_len, unsigned char **digest, unsigned int *digest_len) {
 	
 	const EVP_MD *hashType = getHashType(hashTypeStr);
+	if (hashType == NULL){
+		std::cout << "Not a valid hash type\n";
+		exit(1);
+	}
 
 	EVP_MD_CTX *mdctx;
 
-	if((mdctx = EVP_MD_CTX_new()) == NULL) {std::cout << "Error computing hash"; }
+	if((mdctx = EVP_MD_CTX_new()) == NULL) {std::cout << "Error computing hash\n"; }
 
-	if(1 != EVP_DigestInit_ex(mdctx, hashType, NULL)) {std::cout << "Error computing hash"; }
+	if(1 != EVP_DigestInit_ex(mdctx, hashType, NULL)) {std::cout << "Error computing hash\n"; }
 
-	if(1 != EVP_DigestUpdate(mdctx, message, message_len)) {std::cout << "Error computing hash"; }
+	if(1 != EVP_DigestUpdate(mdctx, message, message_len)) {std::cout << "Error computing hash\n"; }
 
-	if((*digest = (unsigned char *)OPENSSL_malloc(EVP_MD_size(hashType))) == NULL) {std::cout << "Error computing hash"; }
+	if((*digest = (unsigned char *)OPENSSL_malloc(EVP_MD_size(hashType))) == NULL) {std::cout << "Error computing hash\n"; }
 
-	if(1 != EVP_DigestFinal_ex(mdctx, *digest, digest_len)) {std::cout << "Error computing hash"; }
+	if(1 != EVP_DigestFinal_ex(mdctx, *digest, digest_len)) {std::cout << "Error computing hash\n"; }
 
 	EVP_MD_CTX_free(mdctx);
 }
